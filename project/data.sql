834b796ee06d97eddc8b6f8f4f61e1a8f9e5a4a3
Riasat Khan <riasatsakif@gmail.com>
2019-08-04 02:54:51 +0600

    sql updated

--- project/data.sql
+++ project/data.sql
@@ -1,51 +1,206 @@
-/*
--- Query: select * from users
-LIMIT 0, 1000
+-- MySQL Workbench Forward Engineering
 
--- Date: 2019-07-14 03:36
-*/
-INSERT INTO `users` (`id`,`name`,`email`,`password`) VALUES (1,'riasat','riasatsakif@gmail.com','123456');
-/*
--- Query: select * from question_set
-LIMIT 0, 1000
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- Date: 2019-07-14 03:37
-*/
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (1,'Depression Scale');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (7,'Anxiety');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (8,'SAD');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (9,'SAC');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (10,'OCD');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (11,'OCD-Scale');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (12,'Social-Avoidance ');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (13,'Depression-Scale');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (14,'Social-Interaction-Scale');
-INSERT INTO `question_set` (`id`,`name_1`) VALUES (15,'Anxiety-Scale');
-/*
--- Query: select * from questions
-LIMIT 0, 1000
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema PSYCHIATRIC_SCREENING
+-- -----------------------------------------------------
 
--- Date: 2019-07-14 03:37
-*/
-INSERT INTO `questions` (`id`,`set_id`,`question`) VALUES (1,7,'who am i');
-INSERT INTO `questions` (`id`,`set_id`,`question`) VALUES (2,7,'where are you');
-INSERT INTO `questions` (`id`,`set_id`,`question`) VALUES (3,1,'what is my name');
-INSERT INTO `questions` (`id`,`set_id`,`question`) VALUES (4,1,'why am i here');
-/*
--- Query: select * from answers
-LIMIT 0, 1000
+-- -----------------------------------------------------
+-- Schema PSYCHIATRIC_SCREENING
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `PSYCHIATRIC_SCREENING` DEFAULT CHARACTER SET utf8 ;
+USE `PSYCHIATRIC_SCREENING` ;
 
--- Date: 2019-07-14 03:38
-*/
-INSERT INTO `answers` (`id`,`set_id`,`value`,`answer`) VALUES (1,7,1,'yes');
-INSERT INTO `answers` (`id`,`set_id`,`value`,`answer`) VALUES (2,7,0,'no');
-INSERT INTO `answers` (`id`,`set_id`,`value`,`answer`) VALUES (3,1,24,'sakif');
-INSERT INTO `answers` (`id`,`set_id`,`value`,`answer`) VALUES (4,1,10,'riasat');
-/*
--- Query: select * from report_breakdown
-LIMIT 0, 1000
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`question_set`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`question_set` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 16
+DEFAULT CHARACTER SET = latin1;
 
--- Date: 2019-07-14 03:39
-*/
-INSERT INTO `report_breakdown` (`id`,`user_id`,`question_set_id`,`question_id`,`answer_id`) VALUES (3,1,7,1,1);
-INSERT INTO `report_breakdown` (`id`,`user_id`,`question_set_id`,`question_id`,`answer_id`) VALUES (4,1,7,2,1);
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`answers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`answers` (
+  `id` INT(11) NOT NULL,
+  `set_id` INT(11) NOT NULL,
+  `value` INT(11) NOT NULL,
+  `answer` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fkIdx_26` (`set_id` ASC),
+  CONSTRAINT `FK_26`
+    FOREIGN KEY (`set_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`question_set` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`city`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`city` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`areas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`areas` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  `city_id` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `city_id` (`city_id` ASC),
+  CONSTRAINT `areas_ibfk_1`
+    FOREIGN KEY (`city_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`city` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  `email` VARCHAR(200) NULL DEFAULT NULL,
+  `password` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email` (`email` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`auth_log`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`auth_log` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NULL DEFAULT NULL,
+  `token` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_id` (`user_id` ASC),
+  CONSTRAINT `auth_log_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`users` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 33
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`questions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`questions` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `set_id` INT(11) NOT NULL,
+  `question` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fkIdx_15` (`set_id` ASC),
+  CONSTRAINT `FK_15`
+    FOREIGN KEY (`set_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`question_set` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 154
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`report`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`report` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `question_set_id` INT(11) NOT NULL,
+  `score` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fkIdx_53` (`user_id` ASC),
+  INDEX `fkIdx_56` (`question_set_id` ASC),
+  CONSTRAINT `FK_53`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`users` (`id`),
+  CONSTRAINT `FK_56`
+    FOREIGN KEY (`question_set_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`question_set` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 62
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`report_breakdown`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`report_breakdown` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `question_set_id` INT(11) NOT NULL,
+  `question_id` INT(11) NOT NULL,
+  `answer_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fkIdx_40` (`user_id` ASC),
+  INDEX `fkIdx_43` (`question_set_id` ASC),
+  INDEX `fkIdx_46` (`question_id` ASC),
+  INDEX `fkIdx_60` (`answer_id` ASC),
+  CONSTRAINT `FK_40`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`users` (`id`),
+  CONSTRAINT `FK_43`
+    FOREIGN KEY (`question_set_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`question_set` (`id`),
+  CONSTRAINT `FK_46`
+    FOREIGN KEY (`question_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`questions` (`id`),
+  CONSTRAINT `FK_60`
+    FOREIGN KEY (`answer_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`answers` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 940
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `PSYCHIATRIC_SCREENING`.`score`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PSYCHIATRIC_SCREENING`.`score` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `question_set_id` INT(11) NULL DEFAULT NULL,
+  `name` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  `maximum` INT(11) NULL DEFAULT NULL,
+  `minimum` INT(11) NULL DEFAULT NULL,
+  `is_appointment_required` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `question_set_id` (`question_set_id` ASC),
+  CONSTRAINT `score_ibfk_1`
+    FOREIGN KEY (`question_set_id`)
+    REFERENCES `PSYCHIATRIC_SCREENING`.`question_set` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 24
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
